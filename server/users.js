f1eaf46bd17b65e8e1d609e288703e455896668a
ykn9080 <ykn9080@empal.com>
2021-12-15 17:10:42 +0900

    fixed exit

--- server/users.js
+++ server/users.js
@@ -3,24 +3,28 @@ const users = [];
 const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
+  revisit = false;
 
-  const existingUser = users.find((user) => user.room === room && user.name === name);
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+  if (existingUser) revisit = true;
+  if (!name || !room) return { error: "Username and room are required." };
+  //if(existingUser) return { error: 'Username is taken.' };
 
-  if(!name || !room) return { error: 'Username and room are required.' };
-  if(existingUser) return { error: 'Username is taken.' };
-
-  const user = { id, name, room };
-
-  users.push(user);
+  const user = { id, name, room, revisit };
+  if (!existingUser) users.push(user);
 
   return { user };
-}
+};
 
 const removeUser = (id) => {
+  console.log(id, "exited...");
+
   const index = users.findIndex((user) => user.id === id);
 
-  if(index !== -1) return users.splice(index, 1)[0];
-}
+  if (index !== -1) return users.splice(index, 1)[0];
+};
 
 const getUser = (id) => users.find((user) => user.id === id);
 
